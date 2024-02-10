9308e14cb73fce3481fb41f598edc355f895685c
colemgreer <colemgreer@gmail.com>
2024-02-10 18:27:12 -0500

    shape genration and display

--- GUI/UI.py
+++ GUI/UI.py
@@ -19,11 +19,22 @@ dropdown.set("circle")  # Set the default option
 dropdown.pack()
 button.pack()
 
+'''
 listbox = tk.Listbox(root)
 listbox.pack(fill=tk.BOTH, expand=1)
 
 for z in range(40, -1, -1):
     listbox.insert(tk.END, z)
+'''
+canvas = tk.Canvas(root, width=400, height=400)
 
+if button_click:
+    canvas.delete("all")
+    if dropdown.get() == "circle":
+        canvas.create_oval(100, 100, 300, 300, fill="red")
+    elif dropdown.get() == "square":
+        canvas.create_rectangle(100, 100, 300, 300, fill="red")
+    elif dropdown.get() == "triangle":
+        canvas.create_polygon(100, 100, 300, 100, 200, 300, fill="red")
 
 root.mainloop()
