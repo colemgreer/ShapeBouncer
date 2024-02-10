2d0b9550b47d5ef502418d49c082ab98fff19abb
Adam Wright <adambomb320@gmail.com>
2024-02-10 13:55:29 -0500

    tweaked createShape()

--- GUI/Ball.py
+++ GUI/Ball.py
@@ -2,16 +2,17 @@ import turtle
 from turtle import *
 import tkinter as tk
 
-window = tk.Tk()
+
 
 #forward(100)
 
 
 #Function that creates a red ball
 def createShape():
+    window = tk.Tk();
     ball = turtle.Turtle();
     ball.shape("square");
     ball.color("red");
+    window.mainloop();
 
 createShape()
-window.mainloop()
