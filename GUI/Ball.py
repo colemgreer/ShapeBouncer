10199ccb5cc2cc7af63192fecc64813ef93afc20
Adam Wright <adambomb320@gmail.com>
2024-02-10 12:02:04 -0500

    Created Red Ball Function

--- GUI/Ball.py
+++ GUI/Ball.py
@@ -1,8 +1,17 @@
+import turtle
 from turtle import *
 import tkinter as tk
 
 window = tk.Tk()
 
-forward(100)
+#forward(100)
 
+
+#Function that creates a red ball
+def createBall():
+    ball = turtle.Turtle();
+    ball.shape("circle");
+    ball.color("red");
+
+createBall()
 window.mainloop()
